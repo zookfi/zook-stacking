79d1f1ea98c3efc0fa9a383fdbcc0119bda4f927
Zook <144564752+zookfi@users.noreply.github.com>
2023-11-12 11:01:15 +0530

    Update zook-staking-contract.sol

--- zook-staking-contract.sol
+++ zook-staking-contract.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract JookStaking {
+contract ZookStaking {
     using SafeERC20 for IERC20;
 
     uint256 public totalStakedAmount;
